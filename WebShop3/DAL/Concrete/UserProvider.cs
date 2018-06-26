ed29b305d13b7c641098cabdf5a58510984ad154
Soul_Reaver <jepik_ak15@nuwm.edu.ua>
2018-06-26 19:06:55 +0300

    Add UserProvider. Add RegisterUser method to UserProvider

--- WebShop3/DAL/Concrete/UserProvider.cs
+++ WebShop3/DAL/Concrete/UserProvider.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SimpleCrypto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
 using System.Web;
 using WebShop3.DAL.Abstract;
 using WebShop3.DAL.Entities;
@@ -16,12 +18,26 @@ namespace WebShop3.DAL.Concrete
             _context = context;
         }
         public User RegisterUser(RegisterUserViewModel user)
-        {
-            var el = _context.Users.SingleOrDefault(m => m.Email == user.Email);
-            if (el != null)
-                return null;
+        {   
+            using (TransactionScope scope = new TransactionScope())
+            {
+                var el = _context.Users.SingleOrDefault(m => m.Email == user.Email);
+                if (el != null)
+                    return null;
 
-            return _context.Users.Add(new User { FullName = user.FullName });
+                ICryptoService cryptoService = new PBKDF2();
+                string passwordSalt = cryptoService.GenerateSalt();
+                string hashedPassword = cryptoService.Compute(user.Password);
+                Role userRole = _context.Roles.SingleOrDefault(m => m.Name == "User");
+                User newUser = new User { FullName = user.FullName, Email = user.Email, Password = hashedPassword, PasswordSalt = passwordSalt };
+                if(userRole!=null)
+                {
+                    newUser.Roles.Add(userRole);
+                }
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+                scope.Complete();
+                return newUser;
+            }
         }
-    }
 }
