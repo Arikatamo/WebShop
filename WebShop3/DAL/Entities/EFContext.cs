32c6817a95c1f82e654abfcdbfba62c2a9d4833b
Soul_Reaver <jepik_ak15@nuwm.edu.ua>
2018-06-26 18:28:56 +0300

    Add Folder DAL and EFContext

---
+++ WebShop3/DAL/Entities/EFContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebShop3.DAL.Entities
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("DefaultConnection") {}
+    }
+}
